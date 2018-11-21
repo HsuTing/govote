a2b6251ad0ec50a16dab6707768fda7b46ec7e4f
HsuTing <hsuting0106@gmail.com>
2018-11-21 18:14:18 +0800

    feat(components): add Map

---
+++ src/components/Map.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import PropTypes from 'prop-types';
+import { graphql, createFragmentContainer } from 'react-relay';
+
+import { TRANSPORTATION_ARRAY } from 'utils/constants';
+
+class Map extends React.PureComponent {
+  static propTypes = {
+    area: PropTypes.arrayOf(PropTypes.shape({}).isRequired).isRequired,
+    statistics: PropTypes.shape({}).isRequired,
+  };
+
+  componentDidMount() {
+    const {
+      area,
+      statistics: { total },
+    } = this.props;
+    const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiaHN1dGluZyIsImEiOiJRajF4Y0hjIn0.9UDt8uw_fxEX791Styd-lA';
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v9',
+      maxBounds: [[-180, -85], [180, 85]],
+    });
+    const MAX_RADIUS = 30;
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+      className: 'map-pop',
+    });
+
+    map.on('load', () => {
+      area.forEach(({ id, name, value, list }) => {
+        const coordinates = {
+          本島: [120.58, 23.58],
+          大洋洲: [175, -9],
+          亞洲: [87.19, 43.4],
+          北美洲: [-111.725, 48.33],
+          南美洲: [-60, -35],
+          歐洲: [28.4, 53.9],
+          非洲: [16.96, 1.35],
+        }[name];
+
+        map.addSource(id, {
+          type: 'geojson',
+          data: {
+            type: 'Point',
+            coordinates,
+          },
+        });
+
+        map.addLayer({
+          id,
+          source: id,
+          type: 'circle',
+          paint: {
+            'circle-radius': MAX_RADIUS * (value / total),
+            'circle-color': 'rgba(0, 80, 179, 0.3)',
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#0050b3',
+          },
+        });
+
+        map.on('mouseenter', id, e => {
+          map.getCanvas().style.cursor = 'pointer';
+          popup
+            .setLngLat(coordinates)
+            .setHTML(
+              renderToString(
+                <>
+                  <div>{name}</div>
+
+                  <ol>
+                    {list.map(
+                      ({ id: itemId, name: itemName, value: itemValue }) => (
+                        <li key={itemId}>
+                          {itemName}: {itemValue}人
+                        </li>
+                      ),
+                    )}
+                  </ol>
+                </>,
+              ),
+            )
+            .addTo(map);
+        });
+
+        map.on('mouseleave', id, () => {
+          map.getCanvas().style.cursor = '';
+          popup.remove();
+        });
+      });
+    });
+  }
+
+  render() {
+    return <div id="map" />;
+  }
+}
+
+export default createFragmentContainer(
+  Map,
+  graphql`
+    fragment Map_area on Area @relay(plural: true) {
+      id
+      name
+      value
+      list {
+        id
+        name
+        value
+      }
+    }
+
+    fragment Map_statistics on Statistics {
+      total
+    }
+  `,
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-relay';
 import { Button, Row, Col, Carousel } from 'antd';
 
 import Header from 'components/Header';
 import Statistics from 'components/Statistics';
+import Map from 'components/Map';
 import Share from 'components/Share';
 import UsersIcon from 'static/UsersIcon.svg';
 import ArrowIcon from 'static/ArrowIcon.svg';
@@ -20,17 +20,10 @@ export default class Index extends React.Component {
       data {
         statistics {
           ...Statistics_statistics
-          total
+          ...Map_statistics
         }
         area {
-          id
-          name
-          value
-          list {
-            id
-            name
-            value
-          }
+          ...Map_area
         }
         transportation {
           id
@@ -48,94 +41,6 @@ export default class Index extends React.Component {
     }
   `;
 
-  componentDidMount() {
-    const {
-      data: {
-        area,
-        statistics: { total },
-      },
-    } = this.props;
-    const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
-    mapboxgl.accessToken =
-      'pk.eyJ1IjoiaHN1dGluZyIsImEiOiJRajF4Y0hjIn0.9UDt8uw_fxEX791Styd-lA';
-
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/light-v9',
-      maxBounds: [[-180, -85], [180, 85]],
-    });
-    const MAX_RADIUS = 30;
-    const popup = new mapboxgl.Popup({
-      closeButton: false,
-      closeOnClick: false,
-      className: 'map-pop',
-    });
-
-    map.on('load', () => {
-      area.forEach(({ id, name, value, list }) => {
-        const coordinates = {
-          本島: [120.58, 23.58],
-          大洋洲: [175, -9],
-          亞洲: [87.19, 43.4],
-          北美洲: [-111.725, 48.33],
-          南美洲: [-60, -35],
-          歐洲: [28.4, 53.9],
-          非洲: [16.96, 1.35],
-        }[name];
-
-        map.addSource(id, {
-          type: 'geojson',
-          data: {
-            type: 'Point',
-            coordinates,
-          },
-        });
-
-        map.addLayer({
-          id,
-          source: id,
-          type: 'circle',
-          paint: {
-            'circle-radius': MAX_RADIUS * (value / total),
-            'circle-color': 'rgba(0, 80, 179, 0.3)',
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#0050b3',
-          },
-        });
-
-        map.on('mouseenter', id, e => {
-          map.getCanvas().style.cursor = 'pointer';
-          popup
-            .setLngLat(coordinates)
-            .setHTML(
-              renderToString(
-                <>
-                  <div>{name}</div>
-
-                  <ol>
-                    {list.map(
-                      ({ id: itemId, name: itemName, value: itemValue }) => (
-                        <li key={itemId}>
-                          {itemName}: {itemValue}人
-                        </li>
-                      ),
-                    )}
-                  </ol>
-                </>,
-              ),
-            )
-            .addTo(map);
-        });
-
-        map.on('mouseleave', id, () => {
-          map.getCanvas().style.cursor = '';
-          popup.remove();
-        });
-      });
-    });
-  }
-
   render() {
     const {
       data: { statistics, area, transportation, users },
@@ -150,7 +55,7 @@ export default class Index extends React.Component {
 
           <Row className={styles.map} gutter={16} type="flex">
             <Col lg={18} xs={24}>
-              <div id="map" />
+              <Map statistics={statistics} area={area} />
             </Col>
 
             <Col className={styles.area} lg={6} xs={24}>
