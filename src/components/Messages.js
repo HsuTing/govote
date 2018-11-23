fd888ceded00beecc7470e8c0cee4c130d374055
HsuTing <hsuting0106@gmail.com>
2018-11-23 13:11:36 +0800

    feat(components): add Pagination in Messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql, createFragmentContainer } from 'react-relay';
-import { Button, Carousel } from 'antd';
+import { Button, Carousel, Pagination } from 'antd';
 
 import Img from 'static/MessageImg.svg';
 import ArrowIcon from 'static/ArrowIcon.svg';
@@ -11,12 +11,46 @@ import styles from './styles/messages.less';
 class Messages extends React.Component {
   carouselRef = React.createRef();
 
+  state = {
+    pageIndex: 0,
+  };
+
   static propTypes = {
     users: PropTypes.arrayOf(PropTypes.shape({}).isRequired).isRequired,
   };
 
+  paginationRender = (page, type, originalElement) => {
+    switch (type) {
+      case 'prev':
+        return (
+          <Button
+            shape="circle"
+            icon="left"
+            onClick={() => {
+              this.carouselRef.current.slick.slickPrev();
+            }}
+          />
+        );
+
+      case 'next':
+        return (
+          <Button
+            shape="circle"
+            icon="right"
+            onClick={() => {
+              this.carouselRef.current.slick.slickNext();
+            }}
+          />
+        );
+
+      default:
+        return originalElement;
+    }
+  };
+
   render() {
     const { users } = this.props;
+    const { pageIndex } = this.state;
 
     return (
       <>
@@ -29,6 +63,9 @@ class Messages extends React.Component {
         <Carousel
           ref={this.carouselRef}
           className={styles.carousel}
+          afterChange={current => {
+            this.setState({ pageIndex: current });
+          }}
           slidesToShow={3}
           slidesToScroll={3}
           rows={2}
@@ -57,23 +94,16 @@ class Messages extends React.Component {
           ))}
         </Carousel>
 
-        <div className={styles.buttons}>
-          <Button
-            shape="circle"
-            icon="left"
-            onClick={() => {
-              this.carouselRef.current.slick.slickPrev();
-            }}
-          />
-
-          <Button
-            shape="circle"
-            icon="right"
-            onClick={() => {
-              this.carouselRef.current.slick.slickNext();
-            }}
-          />
-        </div>
+        <Pagination
+          className={styles.buttons}
+          current={Math.floor(pageIndex / 3) + 1}
+          total={users.length}
+          itemRender={this.paginationRender}
+          onChange={page => {
+            this.carouselRef.current.slick.slickGoTo((page - 1) * 3);
+          }}
+          pageSize={6}
+        />
       </>
     );
   }
